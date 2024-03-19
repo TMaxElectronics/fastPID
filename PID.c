7b4a03f518a4f8dc160704d14b52009c94b67ad2
TMaxElectronics <60486146+TMaxElectronics@users.noreply.github.com>
2024-03-19 03:27:48 +0100

    Made it work, added output low pass filter

--- PID.c
+++ PID.c
@@ -4,11 +4,15 @@
 #include "FreeRTOS.h"
 
 #include "PID.h"
+#include "System.h"
 
+static uint32_t PID_scaleOutput(PID_Handle_t * pid, int32_t output);
 
 PID_Handle_t * PID_create(int32_t pTerm, int32_t iTerm, int32_t dTerm){
 	PID_Handle_t * pid = pvPortMalloc(sizeof(PID_Handle_t));
 	
+    PID_setCoefficients(pid, pTerm, iTerm, dTerm);
+    PID_setOutputFilter(pid, 0);
 	PID_reset(pid);
 	
 	return pid;
@@ -34,10 +38,20 @@ void PID_setOutputLimits(PID_Handle_t * pid, int32_t outputMax, int32_t outputMi
 	pid->outSpan = outputMax - outputMin;
 }
 
+void PID_setOutputFilter(PID_Handle_t * pid, uint32_t filterRatio){
+	pid->lpFilterRatioA = PID_LP_SUM-filterRatio;
+	pid->lpFilterRatioB = filterRatio;
+    
+    PID_reset(pid);
+}
+
+//PID Coefficients are given in signed 16bit fixed point, with the first three bits being the digit before the decimal point. (so 32767 = 8.0)
 void PID_setCoefficients(PID_Handle_t * pid, int32_t pTerm, int32_t iTerm, int32_t dTerm){
 	pid->pTerm = pTerm;
 	pid->iTerm = iTerm;
 	pid->dTerm = dTerm;
+    
+    PID_reset(pid);
 }
 
 int32_t PID_getCurrentOutput(PID_Handle_t * pid){
@@ -51,15 +65,13 @@ int32_t PID_run(PID_Handle_t * pid, int32_t targetValue, int32_t actualValue){
 	//limit error to 16bit to prevent overflow during further calculations
 	if(error > INT16_MAX) error = INT16_MAX;
 	if(error < INT16_MIN) error = INT16_MIN;
-
-	
-	
+                        
 	//do I calculation
 
 	//check if we can integrate without overflowing the accumulator
 	if(INT32_MAX - abs(pid->accumulator) > abs(error)){ 
 		//yes => do so
-		pid->accumulator += (pid->iTerm * error) >> 16;
+		pid->accumulator += (pid->iTerm * error) >> 10;
 	}else{
 		//no => at least get the acumulator to its maximum value
 		if(error > 0){
@@ -87,17 +99,43 @@ int32_t PID_run(PID_Handle_t * pid, int32_t targetValue, int32_t actualValue){
 	
 	//add terms together
 	int32_t output = 0;
-	output += (pid->pTerm * error) >> 16;
-	output += pid->accumulator >> 16;
-	output += (pid->dTerm * errorDifference) >> 16;
+	output += (pid->pTerm * error) >> 10;
+	output += pid->accumulator;
+	output += (pid->dTerm * errorDifference) >> 10;
+    
+    //TERM_printDebug(TERM_handle, "\rRunning PID: target=%d current=%d error=%d acc=%d out=%d!", targetValue, actualValue, error, pid->accumulator, output);
 	
 	//limit output to 16bit to prevent overflow during further calculations
 	if(output > INT16_MAX) output = INT16_MAX;
 	if(output < INT16_MIN) output = INT16_MIN;
 	
 	//scale into the selected output range
-	int32_t outputScaled = ((pid->outSpan * (output + INT16_MIN)) >> 16) + pid->outMin;
+	int32_t outputScaled = PID_scaleOutput(pid, output);
+    
+    //TERM_printDebug(TERM_handle, "\rRunning PID: target=%d current=%d error=%d acc=%d out=%d outScaled=%d!", targetValue, actualValue, error, pid->accumulator, output, outputScaled);
 	
-	pid->currentOutput = outputScaled;
+    //is the output lowpass enabled?
+	if(pid->lpFilterRatioB > 0){
+        pid->currentOutput = ((outputScaled * pid->lpFilterRatioA) + (pid->currentOutput * pid->lpFilterRatioB)) >> PID_LP_SHIFT;
+    }else{
+        pid->currentOutput = outputScaled;
+    }
+    return pid->currentOutput;
+}
+
+static uint32_t PID_scaleOutput(PID_Handle_t * pid, int32_t output){
+    
+	//limit output to 16bit to prevent overflow during further calculations
+	if(output > INT16_MAX) output = INT16_MAX;
+	if(output < INT16_MIN) output = INT16_MIN;
+    
+    uint32_t outputWithNoOffset = (uint32_t) (output - INT16_MIN);
+    int32_t scaledToSpan = ((pid->outSpan * outputWithNoOffset) >> 16);
+	
+	//scale into the selected output range
+	int32_t outputScaled = scaledToSpan + pid->outMin + 1;
+    
+    //TERM_printDebug(TERM_handle, "Scaling value: val=%d, noOffset=%d, scaled=%d, offset=%d (span=%d min=%d)\r\n", output, outputWithNoOffset, scaledToSpan, outputScaled, pid->outSpan, pid->outMin);
+    
 	return outputScaled;
 }
--- include/PID.h
+++ include/PID.h
@@ -3,14 +3,20 @@
 
 #include <stdint.h>
 
+#define PID_LP_SUM 16
+#define PID_LP_SHIFT 4
+
 typedef struct{
 	int32_t currentOutput;
 	int32_t lastError;
 	int32_t accumulator;
 	
+	int32_t lpFilterRatioA;
+	int32_t lpFilterRatioB;
+    
 	int32_t outMin;
 	int32_t outMax;
-	int32_t outSpan;
+	uint32_t outSpan;
 	
 	int32_t pTerm;
 	int32_t iTerm;
@@ -24,5 +30,6 @@ void PID_setOutputLimits(PID_Handle_t * pid, int32_t outputMax, int32_t outputMi
 void PID_setCoefficients(PID_Handle_t * pid, int32_t pTerm, int32_t iTerm, int32_t dTerm);
 int32_t PID_run(PID_Handle_t * pid, int32_t targetValue, int32_t actualValue);
 int32_t PID_getCurrentOutput(PID_Handle_t * pid);
+void PID_setOutputFilter(PID_Handle_t * pid, uint32_t filterRatio);
 
 #endif
